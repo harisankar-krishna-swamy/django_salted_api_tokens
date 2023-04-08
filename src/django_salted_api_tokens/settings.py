93e2875c59504deb052ef802d489a316984802f3
K.Harisankar <harisankar.krishna@outlook.com>
2023-04-09 07:25:19 +0800

    fix naming

--- src/django_salted_api_tokens/settings.py
+++ src/django_salted_api_tokens/settings.py
@@ -6,6 +6,6 @@ DSAT_X_API_TOKEN_HEADER_NAME = 'AUTHORIZATION'
 DSAT_TOKEN_ID_LENGTH = 40
 
 # Configurable settings from Django project
-DSAT_TOKEN_LENGTH = getattr(settings, 'DEFAULT_TOKEN_LENGTH', 80)
-DSAT_MAX_TOKENS_PER_USER = getattr(settings, 'MAX_TOKENS_PER_USER', 10)
-DSAT_HASHLIB_ALGO = getattr(settings, 'HASH_FUNC', 'sha512')
+DSAT_TOKEN_LENGTH = getattr(settings, 'DSAT_TOKEN_LENGTH', 80)
+DSAT_MAX_TOKENS_PER_USER = getattr(settings, 'DSAT_MAX_TOKENS_PER_USER', 10)
+DSAT_HASHLIB_ALGO = getattr(settings, 'DSAT_HASHLIB_ALGO', 'sha512')
