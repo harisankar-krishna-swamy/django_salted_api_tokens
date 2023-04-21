76f151e2ba22ae8bdd318921f61b8afc81fb9a58
K.Harisankar <harisankar.krishna@outlook.com>
2023-04-21 10:56:15 +0800

    factor out create token for user

--- src/django_salted_api_tokens/views.py
+++ src/django_salted_api_tokens/views.py
@@ -1,13 +1,8 @@
-from ksuid import ksuid
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
-from .models import ApiToken
 from .serializers import TokenResponseSerializer
-from .settings import (DSAT_HASHLIB_ALGO, DSAT_MAX_TOKENS_PER_USER,
-                       DSAT_TOKEN_LENGTH)
-from .utils import gen_token, hashed_secret
+from .utils import create_token_for_user
 
 
 class CreateApiToken(ObtainAuthToken):
@@ -21,16 +16,8 @@ class CreateApiToken(ObtainAuthToken):
         )
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        n_user_tokens = ApiToken.objects.filter(user=user).count()
-        if n_user_tokens >= DSAT_MAX_TOKENS_PER_USER:
-            raise ValidationError('Maximum tokens limit reached for user')
-        token_id, user_token = f'{ksuid()}', gen_token(n_chars=DSAT_TOKEN_LENGTH)
-        db_token, created = ApiToken.objects.get_or_create(
-            user=user,
-            token_id=token_id,
-            token=hashed_secret(secret=user_token, algo=DSAT_HASHLIB_ALGO),
-        )
+        token_id, user_token = create_token_for_user(user)
         token_resp = TokenResponseSerializer(
-            {'token_id': db_token.token_id, 'token': user_token}
+            {'token_id': token_id, 'token': user_token}
         ).data
         return Response(token_resp)
